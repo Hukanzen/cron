53071dd8ce88ecc437ecfe8c004e32131b8b17c3
Hukanzen <qazxswed1934@gmail.com>
2018-06-27 18:26:54 +0900

    macにインストール

--- docker_python3.sh
+++ docker_python3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#SCRIPT_DIR=$(cd $(dirname $0); pwd)
+SCRIPT_DIR=$(cd $(dirname -- $0); pwd)
 
 docker run --rm -i --pid=host -e "TZ=Asia/Tokyo" -v $SCRIPT_DIR:/work -v $HOME/anykeydata:/anykeydata slackweb python3 /work/$1
